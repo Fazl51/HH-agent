1c11c217cda38179dc48fda2c49207ed3f10cda9
Fazl51 <68388385+Fazl51@users.noreply.github.com>
2026-01-18 01:25:38 +0400

    Agent HH

---
+++ main.js
@@ -0,0 +1,12 @@
+﻿import { addResponseBtn } from './modules/interface/addResponseBtn';
+import { addSponsorBtn } from './modules/interface/addSponsorBtn';
+import { addSettingsPanel } from './modules/interface/addSettingsPanel';
+import { autoRaiseResume } from './utils/autoRaiseResume';
+
+(async function main() {
+  await addResponseBtn();
+  await addSponsorBtn();
+
+  addSettingsPanel();
+  await autoRaiseResume();
+})();
